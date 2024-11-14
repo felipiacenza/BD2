be413667c76d9ae19583def747a4cc8f6cdd60e3
Feli Piacenza <piacenzafelipe@gmail.com>
2024-11-14 00:08:15 -0300

    cambios en los indices

--- OBL2/PARTE2/Indices/index.js
+++ OBL2/PARTE2/Indices/index.js
@@ -1,5 +1,21 @@
-db.aplicacion.createIndex({ "nombre": 1 },{ unique: true });
-db.idioma.createIndex({ "nombre": 1 },{ unique: true });
-db.pais.createIndex({ "nombre_pais": 1 }, { unique: true });
-db.usuario.createIndex({ "email": 1 },{ unique: true, name: "unique_email_index" });
+db.aplicacion.createIndex(
+    { nombre: 1 },
+    { unique: true, name: "unique_nombre_aplicacion" }
+);
+db.idioma.createIndex(
+    { nombre: 1 },
+    { unique: true, name: "unique_nombre_idioma" }
+);
+db.pais.createIndex(
+    { nombre_pais: 1 },
+    { unique: true, name: "unique_nombre_pais" }
+);
+db.usuario.createIndex(
+    { email: 1 },
+    { unique: true, name: "unique_email_usuario" }
+);
 
+db.usuario.createIndex(
+    { telefono: 1 },
+    { unique: true, name: "unique_telefono_usuario" }
+);
